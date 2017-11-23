33924a6f4282d21ffec4dded44bf8a46d9a5a9c1
r611171 <edward.cheadle@cambiahealth.com>
2017-11-22 18:58:55 -0700

    added the abilty to use env var for the port

--- microservice.go
+++ microservice.go
@@ -3,14 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
 }
 
-func index(w http.ResponseWriter, *http.Request) {
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello Cloud Native Go.")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
