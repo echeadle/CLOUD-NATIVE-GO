5d316637750052e32ee24c3c68689862e848dc3e
r611171 <edward.cheadle@cambiahealth.com>
2017-11-24 08:44:24 -0700

    Added echo and hello to api

--- api/book.go
+++ api/book.go
@@ -7,10 +7,50 @@ import (
 
 //Book type with Name, Author and ISBN
 type Book struct {
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	ISBN   string `json:"isbn"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	ISBN        string `json:"isbn"`
+	Description string `json:"description,omiempty`
 	//define the book
+}
+
+var books = map[string]Book{
+	"0345391802": Book{Title: "The Hitchhiker's Guide to the Galaxy", Author: "Douglas Admin", ISBN: "0345391802"},
+	"0000000000": Book{Title: "Cloud Native Go", Author: "M. -L. Reimer", ISBN: "0000000000"},
+}
+
+// BooksHandleFunc to be used as http.HandlFunc for Book API
+func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
+	switch method := r.Method; method {
+	case http.MethodGet:
+		books := AllBooks()
+		writeJSON(w, books)
+	case http.MethodPost:
+		body, err := iotutil
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
+	}
+}
+
+func BookHandleFunc(w http.ResponseWriter, r *http.Request) {
+	b, err := json.Marshal(Books)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
+}
+
+func AllBooks() []Book {
+	values := make([]Book, len(books))
+	idx := 0
+	for _, book := range books {
+		values[idx] = book
+		idx++
+	}
+	return values
 }
 
 //ToJSON to be used for marshalling of Book type
@@ -38,12 +78,11 @@ var Books = []Book{
 	Book{Title: "Cloud Native Go", Author: "M. -L. Reimer", ISBN: "0123456789"},
 }
 
-func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
-	b, err := json.Marshal(Books)
+func writeJSON(w http.ResponseWriter, i interface{}) {
+	b, err := json.Marshal(i)
 	if err != nil {
 		panic(err)
 	}
-
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 	w.Write(b)
 }
--- microservice.go
+++ microservice.go
@@ -10,9 +10,10 @@ import (
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/api/echo", echo)
+	http.HandleFunc("/api/hello", api.HelloHandleFunc)
 
 	http.HandleFunc("/api/books", api.BooksHandleFunc)
-	http.HandleFunc("/api/books/", api.BooksHandleFunc)
+	http.HandleFunc("/api/books/", api.BookHandleFunc)
 
 	http.ListenAndServe(port(), nil)
 }
