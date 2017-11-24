bfa359d6317b5db514620cc6e7c7b34d6d3001bb
r611171 <edward.cheadle@cambiahealth.com>
2017-11-24 11:12:07 -0700

    Finished Simple Book API

--- api/book.go
+++ api/book.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -26,7 +27,20 @@ func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
 		books := AllBooks()
 		writeJSON(w, books)
 	case http.MethodPost:
-		body, err := iotutil
+		body, err := ioutil.ReadAll(r.Body)
+		println(body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		book := FromJSON(body)
+		isbn, created := CreateBook(book)
+		if created {
+			w.Header().Add("Location", "api/books/"+isbn)
+			w.WriteHeader(http.StatusCreated)
+		} else {
+			w.WriteHeader(http.StatusConflict)
+		}
+
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Unsupported request method."))
@@ -34,13 +48,35 @@ func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func BookHandleFunc(w http.ResponseWriter, r *http.Request) {
-	b, err := json.Marshal(Books)
-	if err != nil {
-		panic(err)
-	}
+	isbn := r.URL.Path[len("/api/books/"):]
 
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.Write(b)
+	switch method := r.Method; method {
+	case http.MethodGet:
+		book, found := GetBook(isbn)
+		if found {
+			writeJSON(w, book)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	case http.MethodPut:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		book := FromJSON(body)
+		exists := UpdateBook(isbn, book)
+		if exists {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	case http.MethodDelete:
+		DeleteBook(isbn)
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
+	}
 }
 
 func AllBooks() []Book {
@@ -85,4 +121,34 @@ func writeJSON(w http.ResponseWriter, i interface{}) {
 	}
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 	w.Write(b)
+}
+
+// GetBook returns the book for a given ISBN
+func GetBook(isbn string) (Book, bool) {
+	book, found := books[isbn]
+	return book, found
+}
+
+// CreateBook creates a new Book if it does not exist
+func CreateBook(book Book) (string, bool) {
+	_, exists := books[book.ISBN]
+	if exists {
+		return "", false
+	}
+	books[book.ISBN] = book
+	return book.ISBN, true
+}
+
+// UpdateBook updates an existing book
+func UpdateBook(isbn string, book Book) bool {
+	_, exists := books[isbn]
+	if exists {
+		books[isbn] = book
+	}
+	return exists
+}
+
+// DeleteBook removes a book from the map by ISBN key
+func DeleteBook(isbn string) {
+	delete(books, isbn)
 }
